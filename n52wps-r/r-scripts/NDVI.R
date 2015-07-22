605312e010ad874b004c01b03e9a6df28ad6a596
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-07-22 19:59:50 +0200

    NDVI with geotiff save file, return by filename

--- n52wps-r/r-scripts/NDVI.R
+++ n52wps-r/r-scripts/NDVI.R
@@ -1,22 +1,21 @@
 # wps.des: NDVI, title = NDVI-calculator, abstract = Calculates NDVI from two channels in raster image bd_RED and bd_IR are the band numbers;
-# wps.in: image, geotiff_image, input geotiff image;
+# wps.in: image, geotiff, inputgeotiffimage;
 # wps.in: bd_RED, integer, value = 1, red band channel number;
 # wps.in: bd_IR, integer, value = 4, ir band channel number;
 
 require("raster")
 
+# TODO how to check bands?
 rc<-raster(image,band=bd_RED)
-
-# TODO how to check?
-# if (nbands(rc) < max(bd_RED,bd_IR)){
-#         stop("Number of bands to low!") 
-#         }
-
 irc<-raster(image,band=bd_IR)
+
 NDVI <- overlay(
                 rc,
                 irc,
                 fun=function(x,y){(y-x)/(y+x)}
                 )
 
-# wps.out: NDVI, geotiff_image, output geotiff;
+NDVIfile=tempfile(pattern = "NDVI_r_wps", tmpdir = tempdir(), fileext = ".tiff")
+writeRaster(NDVI, output=NDVIfile,"GTiff",overwrite=TRUE)
+
+# wps.out: NDVIfile, geotiff, outputgeotiff;
