3a6eb5981ba6e6433f173d98bfb9a776bf0d216d
Martin Weis <Martin.Weis@hft-stuttgart.de>
2015-07-30 18:15:00 +0200

    wps.in integers corrected (bd_)

--- n52wps-r/r-scripts/NDVI.R
+++ n52wps-r/r-scripts/NDVI.R
@@ -1,7 +1,8 @@
 # wps.des: NDVI, title = NDVI-calculator, abstract = Calculates NDVI from two channels in raster image bd_RED and bd_IR are the band numbers;
-# wps.in: image, geotiff, inputgeotiffimage;
-# wps.in: bd_RED, integer, value = 1, red band channel number;
-# wps.in: bd_IR, integer, value = 4, ir band channel number;
+# wps.in: image, geotiff_image, inputgeotiffimage;
+
+# wps.in: bd_RED, integer, abstract =red band channel number, value =1;
+# wps.in: bd_IR, integer, abstract =ir band channel number, value =4;
 
 require("raster")
 
@@ -18,4 +19,4 @@ NDVI <- overlay(
 NDVIfile=tempfile(pattern = "NDVI_r_wps", tmpdir = tempdir(), fileext = ".tiff")
 writeRaster(NDVI, output=NDVIfile,"GTiff",overwrite=TRUE)
 
-# wps.out: NDVIfile, geotiff, outputgeotiff;
+# wps.out: NDVIfile, geotiff_image, outputgeotiff;
